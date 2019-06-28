3a31ad9f4a18dc30c5b3e95c59c740751e14ac4a
addy0101 <adarshg789@gmail.com>
2019-06-28 13:39:06 +0530

    Converitng an integer into individual digits

---
+++ Integer2digits.py
@@ -0,0 +1,10 @@
+### Convert an integer into single digits 
+
+a = 12345
+
+ans = list(map(int,str(a)))
+
+print('given integer is %d'%(a),'\n','output is', ans)
+
+
+# Since a string object in python is iterable. 
